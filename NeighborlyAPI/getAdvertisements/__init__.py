73cac189ca322b1bb9c429bdf0e7b363c4b8aaee
bharat <34162174+blurredart@users.noreply.github.com>
2022-11-21 08:00:48 +0530

    neigbourly project frontend and backend hosted using Azure services

---
+++ NeighborlyAPI/getAdvertisements/__init__.py
@@ -0,0 +1,24 @@
+import azure.functions as func
+import pymongo
+import json
+import os
+from bson.json_util import dumps
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+
+    try:
+        url = os.environ["MyDbConnection"]
+        client = pymongo.MongoClient(url)
+        database = client['neighbourly-data']
+        collection = database['adverts']
+
+
+        result = collection.find({})
+        result = dumps(result)
+
+        return func.HttpResponse(result, mimetype="application/json", charset='utf-8')
+    except:
+        print("could not connect to mongodb")
+        return func.HttpResponse("could not connect to mongodb",
+                                 status_code=400)
+
